8876a0bbc9440d23a05f96fd072060dd31391c82
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-06-09 11:19:27 +0300

    fixes the localStorage initial parse issue

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { createStore } from 'redux';
 import { devToolsEnhancer } from '@redux-devtools/extension';
 
+const storedContacts = JSON.parse(localStorage.getItem('contacts'));
+
 const initialState = {
-  contacts: JSON.parse(localStorage.getItem('contacts')) ?? [
+  contacts: storedContacts ?? [
     { id: 'id-1', name: 'Kyivstar', number: '0 800 300 466' },
     { id: 'id-2', name: 'Lifecell', number: '0 800 20 5433' },
     { id: 'id-3', name: 'Vodafone', number: '0 800 400 111' },
